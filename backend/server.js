7a9e5398ddef37ce8bc4e061c59157f520683168
hetpatel4381 <hetpatel2808hp@gmail.com>
2024-01-18 18:22:10 +0530

    connection to MongoDB is successful

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server started at port: ${PORT}`);
+const connectMongoDB = "mongodb://0.0.0.0:27017/book-store/books";
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Welcome to MERN stack Application");
 });
+
+mongoose
+  .connect(connectMongoDB)
+  .then(() => {
+    console.log(`Database is connected at: ${connectMongoDB}`);
+    app.listen(PORT, () => {
+      console.log(`Server started at port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to Database", error);
+  });
